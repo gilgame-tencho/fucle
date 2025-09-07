72a7735204a8b1945c9bed37f053db93aaf309ec
gilgame-tencho <tokubetugosetuchitai.ttiw@gmail.com>
2025-09-08 08:31:59 +0900

    add coin hex frame

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// ルートURLにアクセスしたときの処理
 app.get("/", (req, res) => {
 //   res.send("Hello World! Node.js Web App is running");
-  console.log("__dirname is");
-  console.log(__dirname);
-
-  
   res.sendFile(path.join(__dirname, "menu.html"));
+});
+
+app.get("/randomness/coin_hex", (req, res) => {
+//   res.send("Hello World! Node.js Web App is running");
+  res.sendFile(path.join(__dirname, "static/randomness/pt_coin_hex.html"));
 });
 
 
